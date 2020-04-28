59671b227449c1257aa440ca2da0ec74441cd1ac
Daniel Sottile <danielsottile@gmail.com>
2020-04-28 17:23:32 -0400

    finished

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-'use strict';
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
@@ -31,36 +30,38 @@ function handleBearer(req, res, next) {
 
 app.get('/movie', (req, res) => {
   const { genre, country, avg_vote } = req.query;
-  const newGenre = genre.toLowerCase();
-  const newCountry = country.toLowerCase();
-  let newMovies = [...movies];
-  let genreArr = [];
-  newMovies.forEach((movie) => {
-    const movieString = movie.toLowerCase();
-    if (!genreArr.includes(movieString)) {
-      genreArr.push(movieString);
-    }
-  });
-  let countryArr = [];
-  movies.forEach((movie) => {
-    const movieString = movie.country.toLowerCase();
-    if (!countryArr.includes(movieString)) {
-      countryArr.push(movieString);
-    }
-  });
-  if (newGenre && !genreArr.includes(newGenre)) {
-    return res.status(400).json({ error: '' });
+
+  let newMovie = [...movies];
+
+
+  if (genre && !['action', 'drama', 'comedy', 'animation', 'thriller', 'crime', 'romantic', 'spy', 'adventure', 'documentary', 'horror', 'history', 'western', 'biography', 'musical', 'fantasy', 'war', 'grotesque'].includes(genre.toLowerCase())) {
+    return res.status(400).json({ error: 'needs to include valid genre' });
   }
-  if (newGenre) {
-    newMovies = newMovies.filter((app) => app.genre.includes(newGenre));
+
+  
+
+  if (genre) {
+    newMovie = newMovie.filter((movie) => movie.genre.includes(genre));
   }
-  if (newCountry && !countryArr.includes(newCountry)) {
-    return res.status(400).json({ error: '' });
+  
+  
+  if (country && !['united states', 'italy', 'great britain', 'japan', 'france', 'germany', 'spain', 'canada', 'china', 'hungary', 'israel'].includes(country.toLowerCase())) {
+    return res.status(400).json({ error: 'needs to include valid country' });
   }
-  if (newCountry) {
-    newMovies = newMovies.filter((app) => app.country.includes(newCountry));
+  if (country) {
+    newMovie = newMovie.filter((app) => app.country.includes(country));
   }
-  res.json(newMovies);
+
+  if (Number(avg_vote) < 0 && Number(avg_vote) > 10){
+    return res.status(400).json({ error: 'needs to include valid genre' });
+  } 
+
+  if (avg_vote) {
+    newMovie = newMovie.filter(movie =>  Number(movie.avg_vote) >= Number(avg_vote));
+  }
+
+
+  res.json(newMovie);
 });
 
 app.listen(8080, () => console.log('Server on 8080'));
